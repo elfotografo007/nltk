9a99f7df9955230935b91ae2a2c204c239ed96ae
Andres Torres <elfotografo007@gmail.com>
2018-06-26 18:59:33 +0200

    Add spacy tokenizer

---
+++ nltk/tokenize/spacy.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from nltk.tokenize.api import TokenizerI
+import spacy
+
+
+class SpacyTokenizer(TokenizerI):
+    """
+    The Spacy tokenizer uses spaCy library to tokenize strings.
+
+    """
+
+    def tokenize(self, s):
+        """
+        Tokenizes string using spaCy library tokenizer.
+
+        :param s: String to tokenize.
+        :type s: str
+        :return: str
+        """
+        nlp = spacy.load('en')
+        doc = nlp(s)
+        return [w.text for w in doc]
